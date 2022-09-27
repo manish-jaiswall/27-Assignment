1e32e940d810e9692c6785b715e3afb14fd46656
Manish Kumar Jaiswal <Manish.Jaiswal@ivycomptech.com>
2022-09-27 19:20:42 +0545

    Add project files.

---
+++ Assign5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assign5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = "C:\\Users\\manish.jaiswal\\OneDrive - Entain Group\\Documents\\12\\Assign3.txt";
+               StreamReader sr = new StreamReader(path);
+               while(!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("file not found");
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Directory not found");
+            }
+        }
+    }
+}
